4844f7751cf4229618cd48218b3d0aa25180f4dc
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-01-17 21:10:07 -0500

    working on rendering routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import BrowserRouter from 'react-router-dom';
+
+import Login from './components/Login';
+
+import AddFriend from './components/AddFriend';
+
+import FriendsList from './components/FriendsList';
+
+// const Login = () => {
+//   return(<h2>Login</h2>)
+// }
+
+// const AddFriend = () => {
+//   return(<h2>AddFriend</h2>)
+// }
+
+// const FriendsList = () => {
+//   return(<h2>FriendsList</h2>)
+// }
 
 function App() {
   return (
+    <BrowserRouter>
+    <Router>
     <div className="App">
       <h2>Client Auth Project</h2>
+      <Login />
+      <AddFriend />
+      <FriendsList />
+    
     </div>
+    </Router>
+    </BrowserRouter>
   );
 }
 
